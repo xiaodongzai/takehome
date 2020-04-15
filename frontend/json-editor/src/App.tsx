9375eece8eb9d34ecce6fedef4cdc1e6d4811c5e
xiaodongzai <llqintel@163.com>
2020-04-15 21:56:56 +0800

    feat(dev): first commit

---
+++ frontend/json-editor/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Editor from "./components/Editor";
+import "./styles.scss";
+
+export function App() {
+  return (
+    <div>
+      <Editor />
+    </div>
+  );
+}
